1e29361fabb283092e991d2a1d35d2d7b03099fd
Deepak Chaudhary <68501527+DeepakChaudhary47@users.noreply.github.com>
2023-05-21 15:52:27 +0530

    Add files via upload

---
+++ InheritanceS.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+class student {
+    int id, eng, hin, math, comp, art, avg;
+    String name;
+
+    void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter ID:   ");
+        id = sc.nextInt();
+        System.out.print("Enter Name: ");
+        name = sc.next();
+        System.out.println("Marks Of Five Subjects: ");
+        System.out.print("English:    ");
+        eng = sc.nextInt();
+        System.out.print("Hindi:      ");
+        hin = sc.nextInt();
+        System.out.print("Maths:      ");
+        math = sc.nextInt();
+        System.out.print("Physic:     ");
+        comp = sc.nextInt();
+        System.out.print("Chemistry:  ");
+        art = sc.nextInt();
+
+    }
+}
+
+class InheritanceS extends student {
+    void disp() {
+
+        System.out.println("\nResults : ");
+        int avg = (eng + hin + math + comp + art) / 5;
+        int t=eng+hin+math+comp+art;
+        System.out.println("\nRoll No:" + id + "\nName:" + name + "\nTotal Marks:"+t +"\nPercentage:" +avg+"%");
+    }
+
+    public static void main(String[] args) {
+        InheritanceS s = new InheritanceS();
+        s.input();
+        s.disp();
+    }
+}
